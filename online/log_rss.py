c4b37f87dd77c9991a11798b42684f639d0ab90c
leaaaloves <leatumabotabo@gmail.com>
2018-05-14 12:00:37 +0800

    26 Updated log_rss.py

---
+++ online/log_rss.py
@@ -0,0 +1,40 @@
+import logging
+import time
+
+import cflib.crtp
+from cflib.crazyflie import Crazyflie
+from cflib.crazyflie.log import LogConfig
+from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
+from cflib.crazyflie.syncLogger import SyncLogger
+
+logging.basicConfig(level=logging.ERROR)
+
+if __name__=="__main__":
+
+    cflib.crtp.init_drivers(enable_debug_driver=False)
+    print('Scanning interfces for Crazyflies...')
+    available = cflib.crtp.scan_interfaces()
+    print ('Crazyflies found:')
+    for i in available:
+        print(i[0])
+
+        if len(available) == 0:
+            print('No Crazyflies found, cannot run example')
+        else:
+            lg_rss = LogConfig('RSSI', period_in_ms=10)
+            lg_rss.add_variable('radio.rssi', 'float')
+
+            cf = Crazyflie(rw_cache='./cache')
+            with SyncCrazyflie(available[0][0], cf=cf) as scf:
+                with SyncLogger(scf, lg_rss) as logger:
+                    endTime = time.time() + 10
+
+                    for log_entry in logger:
+                        timestamp = log_entry[0]
+                        data = log_entry[1]
+                        logconf_name = log_entry[2]
+
+                        print('[%d][%s]: %s' % (timestamp, logconf_name, data))
+
+                        if time.time() > endTime:
+                            break
